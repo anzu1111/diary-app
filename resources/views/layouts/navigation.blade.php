590f2185b0fae7074369b0840e26dcb41847f16c
tahoito <taho.ito0304@gmail.com>
2026-06-12 22:14:31 +0900

    アイコンエラー修正

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,24 +1,21 @@
 <nav class="fixed bottom-0 left-0 w-full bg-[#EFBD94] rounded-t-[20px] shadow-lg">
     <div class="max-w-md mx-auto grid grid-cols-3 h-20">
 
-        <!-- Home -->
         <a href="/home"
             class="flex items-center justify-center text-text">
-            <x-lucide.home
+            <x-lucide-home
                 class="w-6 h-6 stroke-[1.0]"/>
         </a>
 
-        <!-- Calendar -->
         <a href="/calendar"
             class="flex items-center justify-center text-text">
-            <x-lucide.calendar-days
+            <x-lucide-calendar-days
                 class="w-6 h-6 stroke-[1.0]"/>
         </a>
 
-        <!-- Profile -->
         <a href="/profile"
             class="flex items-center justify-center text-text">
-            <x-lucide.user
+            <x-lucide-user
                 class="w-6 h-6 stroke-[1.0]" />
         </a>
 
