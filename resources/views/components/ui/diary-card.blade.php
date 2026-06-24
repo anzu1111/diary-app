2c1346e2bd5231f899910320caf8ec4a0e017fa0
as <azuha.ssk@icloud.com>
2026-06-24 16:38:22 +0900

    カード追加

---
+++ resources/views/components/ui/diary-card.blade.php
@@ -0,0 +1,8 @@
+<div class="bg-white rounded-[20px] py-4 px-5 font-semibold">
+    <div class="flex items-end gap-2">
+        <span class="text-[28px] leading-none">1</span>
+        <span class="inline-flex w-6 h-6 rounded-full bg-secondary items-center justify-center text-sm">日</span>
+        <span class="text-sm">2026/3</span>
+    </div>
+    <p class="mt-4 text-sm">今日はカフェで勉強した！</p>
+</div>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,8 +1,17 @@
 @extends('layouts.app')
 @section('content')
 
-<div>
-    <p>ホームページです!!</p>
+<div class="min-h-screen">
+    <div class="pl-3">
+        <x-lucide-search class="w-6 h-6 stroke-[1.0]"/>
+    </div>
+    <div class=""></div>
+    <div class="mt-[48px] flex flex-col gap-8">
+        <x-ui.diary-card />
+        <x-ui.diary-card />
+        <x-ui.diary-card />
+        <x-ui.diary-card />
+    </div>
 </div>
 
 @endsection
