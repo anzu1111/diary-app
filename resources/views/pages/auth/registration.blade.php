2abc797a2a95ca1c5bf027c9567467a392b835e7
as <azuha.ssk@icloud.com>
2026-07-20 14:31:29 +0900

    ログイン/新規登録

--- resources/views/pages/auth/registration.blade.php
+++ resources/views/pages/auth/registration.blade.php
@@ -4,8 +4,53 @@
 @section('content')
 <div class="relative flex min-h-screen items-center justify-center overflow-hidden bg-base_color">
     <div class="w-full max-w-md px-4">
+        <form onsubmit="return false;">
+            <div>
+                <label for="email" class="text-[16px] font-semibold">メールアドレス</label>
+                <input
+                    id="email"
+                    type="email"
+                    name="email"
+                    placeholder="example@gmail.com"
+                    autocomplete="off"
+                    class="mt-2 w-full h-[50px] rounded-[10px] bg-white px-4 py-[14px] text-sm font-semiblold border-none outline-none"/>
+            </div>
+            <div class="pt-8">
+                <label for="password" class="text-[16px] font-semibold">パスワード</label>
+                <div class="relative">
+                    <input
+                        id="password"
+                        type="password"
+                        name="password"
+                        placeholder="•••••••••"
+                        autocomplete="off"
+                        class="mt-2 w-full h-[50px] rounded-[10px] bg-white px-4 py-[14px] text-sm font-semiblold border-none outline-none">
+                    </input>
+                    <x-lucide-eye-off class="absolute right-2 top-1/2 w-6 h-6 stroke-[1.0] text-[#3C2415] -translate-y-1/2"/>
+                </div>
+            </div>
+            <div class="pt-8">
+                <label for="password" class="text-[16px] font-semibold">パスワード(確認)</label>
+                <div class="relative">
+                    <input
+                        id="password"
+                        type="password"
+                        name="password"
+                        placeholder="•••••••••"
+                        autocomplete="off"
+                        class="mt-2 w-full h-[50px] rounded-[10px] bg-white px-4 py-[14px] text-sm font-semiblold border-none outline-none">
+                    </input>
+                    <x-lucide-eye-off class="absolute right-2 top-1/2 w-6 h-6 stroke-[1.0] text-[#3C2415] -translate-y-1/2"/>
+                </div>
+            </div>
 
-        
+            <div class="mt-[145px]">
+                <button type="button" class="w-full h-[54px] rounded-[30px] bg-primary text-text text-[16px] font-semibold">新規登録</button>
+            </div>
+            <div class="mt-5 text-center">
+                <a href="{{ route('login') }}" class="text-[14px] font-semibold underline" >ログインの方はこちら</a>
+            </div>
+        </form>
     </div>
 </div>
 @endsection
