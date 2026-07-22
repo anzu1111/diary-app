1087353fa8b2109999c201b158a6ab397fe270d9
tahoito <taho.ito0304@gmail.com>
2026-07-22 12:20:44 +0800

    Controller追加

---
+++ resources/views/pages/calendar.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+@section('content')
+
+<div class="min-h-screen">
+    <div class="flex justify-center">
+        <p class="text-[18px] font-semibold">
+            カレンダー
+        </p>
+    </div>
+
+</div>
+
+@endsection
