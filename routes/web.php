4336b39dd11095869b200fedfbf0f9c7e12d12ea
tahoito <taho.ito0304@gmail.com>
2026-07-28 17:15:32 +0800

    app

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,31 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/auth/login', function () {
+    return view('pages.auth.login');
+})->name('login');
+
+Route::get('/auth/registration', function () {
+    return view('pages.auth.registration');
+})->name('registration');
+
+
+Route::get('/home', function () {
+    return view('pages/home');
+})->name('home');
+
+Route::get('/create', function () {
+    return view('pages.post.create');
+})->name('post.create');
+
+Route::get('/tags', function () {
+    return view('pages.post.tags');
+})->name('post.tags');
+
+
+Route::get('/detail/{id}', function ($id) {
+    return view('pages.detail');
+})->name('detail');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
